97ed45dc12216b721b5a4a6c89e6305c1e43e037
Gary-Bull <62598332+Gary-Bull@users.noreply.github.com>
2020-09-18 22:43:30 +0100

    Add files via upload

---
+++ build/precache-manifest.084a8d516f97271f7da203ff51ccf171.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37b9b4f57130276a67a39805390b1d79",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7e1c3d09337fffd4d97a",
+    "url": "/static/css/main.cead29d5.chunk.css"
+  },
+  {
+    "revision": "56358a45be090a893ae5",
+    "url": "/static/js/2.63c5cdda.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.63c5cdda.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7e1c3d09337fffd4d97a",
+    "url": "/static/js/main.a53ac933.chunk.js"
+  },
+  {
+    "revision": "92a146e5ab9e0f9e785b",
+    "url": "/static/js/runtime-main.90140090.js"
+  }
+]);
